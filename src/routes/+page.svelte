0c5c8a1356a47ca5dc176b390fcd753118f0cef7
Mike Hall <michael.hall@zaptic.com>
2023-10-03 13:40:42 +0100

    Apply prettier

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,35 +1,35 @@
 <script>
-    let quantity = 1;
-    let price = 0;
-    let stateCode = "UT";
+	let quantity = 1;
+	let price = 0;
+	let stateCode = 'UT';
 
-    const taxRateChart = new Map([
-        ["UT", 1.0685],
-        ["NV", 1.08],
-        ["TX", 1.0625],
-        ["AL", 1.04],
-        ["CA", 1.0825]
-    ]);
+	const taxRateChart = new Map([
+		['UT', 1.0685],
+		['NV', 1.08],
+		['TX', 1.0625],
+		['AL', 1.04],
+		['CA', 1.0825]
+	]);
 
-    const discounts = [
-        { threshold: 50000, rate: 0.15 },
-        { threshold: 10000, rate: 0.1 },
-        { threshold: 7000, rate: 0.07 },
-        { threshold: 5000, rate: 0.05 },
-        { threshold: 1000, rate: 0.03 },
-    ];
+	const discounts = [
+		{ threshold: 50000, rate: 0.15 },
+		{ threshold: 10000, rate: 0.1 },
+		{ threshold: 7000, rate: 0.07 },
+		{ threshold: 5000, rate: 0.05 },
+		{ threshold: 1000, rate: 0.03 }
+	];
 
-    const getDiscount = (total) => {
-        const discount = discounts.find(({ threshold }) => threshold <= total);
-        return discount ? (total * discount.rate) : 0;
-    };
+	const getDiscount = (total) => {
+		const discount = discounts.find(({ threshold }) => threshold <= total);
+		return discount ? total * discount.rate : 0;
+	};
 
-    $: total = quantity * price * (taxRateChart.get(stateCode) ?? 1);
-    $: totalWithDiscount = total - getDiscount(total);
+	$: total = quantity * price * (taxRateChart.get(stateCode) ?? 1);
+	$: totalWithDiscount = total - getDiscount(total);
 </script>
 
-<div>Quantity: <input type="number" bind:value={quantity}></div>
-<div>Price: <input type="number" bind:value={price}></div>
-<div>State: <input type="text" bind:value={stateCode}></div>
+<div>Quantity: <input type="number" bind:value={quantity} /></div>
+<div>Price: <input type="number" bind:value={price} /></div>
+<div>State: <input type="text" bind:value={stateCode} /></div>
 
 <p>Total: ${Number(totalWithDiscount).toFixed(2)}</p>
