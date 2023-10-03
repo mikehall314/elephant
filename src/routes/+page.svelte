30670b802d9bc6637bf5af60de6244f7b4bc2a83
Mike Hall <michael.hall@zaptic.com>
2023-10-03 13:03:00 +0100

    feat: Support any tax rate

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,14 @@
 <script>
     let quantity = 1;
     let price = 0;
+    let taxPercent = 0;
 
-    const taxRate = 1.0685
-
+    $: taxRate = 1 + (taxPercent / 100);
     $: total = Number(quantity * price * taxRate).toFixed(2);
 </script>
 
 <div>Quantity: <input type="number" bind:value={quantity}></div>
 <div>Price: <input type="number" bind:value={price}></div>
+<div>Tax Rate: <input type="number" bind:value={taxPercent}></div>
 
 <p>Total: ${total}</p>
