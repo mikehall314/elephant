b48f7d4046bc9b64da784b9c1a9d3d1404cbac4d
Mike Hall <michael.hall@zaptic.com>
2023-10-03 13:09:58 +0100

    feat: Support any state

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,41 +1,22 @@
 <script>
     let quantity = 1;
     let price = 0;
-    let taxPercent = 0;
+    let stateCode = "UT";
 
-    $: taxRate = 1 + (taxPercent / 100);
-    $: total = Number(quantity * price * taxRate).toFixed(2);
+    const taxRateChart = new Map([
+        ["UT", 1.0685],
+        ["NV", 1.08],
+        ["TX", 1.0625],
+        ["AL", 1.04],
+        ["CA", 1.0825]
+    ]);
+
+    $: total = quantity * price * (taxRateChart.get(stateCode) ?? 1);
 </script>
 
 <div>Quantity: <input type="number" bind:value={quantity}></div>
 <div>Price: <input type="number" bind:value={price}></div>
-<div>Tax Rate: <input type="number" bind:value={taxPercent}></div>
+<div>State: <input type="text" bind:value={stateCode}></div>
 
-<p>Total: ${total}</p>
+<p>Total: ${Number(total).toFixed(2)}</p>
 
-<table>
-    <tr>
-        <th>State</th>
-        <th>Rate</th>
-    </tr>
-    <tr>
-        <td>UT</td>
-        <td>6.85%</td>
-    </tr>
-    <tr>
-        <td>NV</td>
-        <td>8.00%</td>
-    </tr>
-    <tr>
-        <td>TX</td>
-        <td>6.25%</td>
-    </tr>
-    <tr>
-        <td>AL</td>
-        <td>4.00%</td>
-    </tr>
-    <tr>
-        <td>CA</td>
-        <td>8.25%</td>
-    </tr>
-</table>
