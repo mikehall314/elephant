90663c26ee54213a0a93e64663961bb7546b7fba
Mike Hall <michael.hall@zaptic.com>
2023-10-03 13:05:26 +0100

    feat: Add state tax chart

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,3 +12,30 @@
 <div>Tax Rate: <input type="number" bind:value={taxPercent}></div>
 
 <p>Total: ${total}</p>
+
+<table>
+    <tr>
+        <th>State</th>
+        <th>Rate</th>
+    </tr>
+    <tr>
+        <td>UT</td>
+        <td>6.85%</td>
+    </tr>
+    <tr>
+        <td>NV</td>
+        <td>8.00%</td>
+    </tr>
+    <tr>
+        <td>TX</td>
+        <td>6.25%</td>
+    </tr>
+    <tr>
+        <td>AL</td>
+        <td>4.00%</td>
+    </tr>
+    <tr>
+        <td>CA</td>
+        <td>8.25%</td>
+    </tr>
+</table>
