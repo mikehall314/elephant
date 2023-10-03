e85199d39321f3de64afe74832a88f2999448a65
Mike Hall <michael.hall@zaptic.com>
2023-10-03 12:56:49 +0100

    feat: Inputs now echo value to screen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,9 @@
-<h1>The Application Works</h1>
+<script>
+    let quantity = 1;
+    let price = 0;
+</script>
+
+<div>Quantity: <input type="number" bind:value={quantity}></div>
+<div>Price: <input type="number" bind:value={price}></div>
+
+<p>Total: {quantity} * ${price}</p>
