3798399092f7124164d5337bb517fbf1dbf0d3c7
Mike Hall <michael.hall@zaptic.com>
2023-10-03 13:00:00 +0100

    feat: Hard-coded tax for Utah

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,13 @@
 <script>
     let quantity = 1;
     let price = 0;
+
+    const taxRate = 1.0685
+
+    $: total = Number(quantity * price * taxRate).toFixed(2);
 </script>
 
 <div>Quantity: <input type="number" bind:value={quantity}></div>
 <div>Price: <input type="number" bind:value={price}></div>
 
-<p>Total: ${quantity * price}</p>
+<p>Total: ${total}</p>
